2b90fa12b3ca882e7d86bb1c22546f56bfbc645e
Nick Maclean <nicolasimaclean@gmail.com>
2022-11-09 15:41:18 -0600

    batman pilot frame

---
+++ Assets/_Game/Scripts/Play/PilotSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Play
+{
+    [CreateAssetMenu(menuName = "Level/Pilot")]
+    public class PilotSO : ScriptableObject
+    {
+        public Sprite Normal;
+        public Sprite Hurt;
+        public AnimationSO Blink;
+    }
+}
